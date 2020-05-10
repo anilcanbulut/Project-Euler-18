8f43ea5f52381d855a7e5b3ceb793a45b2664163
Anılcan BULUT <32616092+anilcanbulut@users.noreply.github.com>
2020-05-10 14:57:19 +0300

    main code for finding the max sum

---
+++ v02-only-sum.py
@@ -0,0 +1,27 @@
+import os
+import numpy as np
+
+with open('file.txt', 'r') as f:
+    lines = f.read().splitlines()		#splitline looks at the line breaks
+    last_line = lines[-1]				#get the last line
+
+    size_last_line = len(last_line.split())		#find the size of last line
+    arr = np.zeros((size_last_line, size_last_line), dtype=int)		#create a 2D square array with same size of the last line
+
+    for i in range(size_last_line):
+    	for j in range(len(lines[i].split())):
+    		word = lines[i].split()
+    		arr[i][j] = word[j]
+
+for i in range(size_last_line):
+	for j in range(size_last_line-i-1):
+
+		if (arr[(size_last_line-1) - i][j] > arr[(size_last_line-1) - i][j+1]):
+			arr[(size_last_line-1) - i - 1][j] += arr[(size_last_line-1) - i][j]
+		else:
+			arr[(size_last_line-1) - i - 1][j] += arr[(size_last_line-1) - i][j+1]
+
+
+print(arr[0][0])
+
+
